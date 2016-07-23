5e0a3c3e7c4ee13618ea41ac93796391be4ebb59
eric.kivet <eric.kivet@gmail.com>
2016-07-23 13:37:23 -0400

    commit #2

--- ToolBarLab/app/src/main/java/com/example/erickivet/toolbarlab/Main2Activity.java
+++ ToolBarLab/app/src/main/java/com/example/erickivet/toolbarlab/Main2Activity.java
@@ -3,7 +3,13 @@ package com.example.erickivet.toolbarlab;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -15,6 +21,47 @@ public class Main2Activity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Toolbar toolbarBottom = (Toolbar)findViewById(R.id.toolbar_bottom_2);
+
+        ImageView playSong = (ImageView)toolbarBottom.findViewById(R.id.play_theme_song_2);
+        ImageView playOtherSong = (ImageView)toolbarBottom.findViewById(R.id.image_bottom_2);
+        TextView someText = (TextView)toolbarBottom.findViewById(R.id.textview_bottom_2);
+        ImageView info = (ImageView)toolbarBottom.findViewById(R.id.display_info_2);
+
+        playSong.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(Main2Activity.this,"You're",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        playOtherSong.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(Main2Activity.this,"Clicking",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        someText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(Main2Activity.this,"Some",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        info.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(Main2Activity.this,"Stuff",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu2_main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -22,6 +69,9 @@ public class Main2Activity extends AppCompatActivity {
         switch(item.getItemId()){
             case android.R.id.home:
                 onNavigateUp();
+                return true;
+            case R.id.action_done:
+                Toast.makeText(Main2Activity.this,"Action Done",Toast.LENGTH_LONG).show();
                 return true;
         }
 
--- ToolBarLab/app/src/main/java/com/example/erickivet/toolbarlab/MainActivity.java
+++ ToolBarLab/app/src/main/java/com/example/erickivet/toolbarlab/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.erickivet.toolbarlab;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,10 +10,12 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +26,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbarBottom = (Toolbar)findViewById(R.id.toolbar_bottom);
         setSupportActionBar(toolbar);
 
+        ImageView playSong = (ImageView)toolbarBottom.findViewById(R.id.play_theme_song);
+        TextView textBottom = (TextView)toolbarBottom.findViewById(R.id.textview_bottom);
+        ImageView displayInfo = (ImageView)toolbarBottom.findViewById(R.id.display_info);
 
+        playSong.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"You Clicked Play Song",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        textBottom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"You Clicked The Text",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        displayInfo.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"You Clicked The Info",Toast.LENGTH_LONG).show();
+            }
+        });
 
     }
-
 
 
     @Override
@@ -36,8 +61,25 @@ public class MainActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
 
     }
+/**
+    @Override
+    public boolean onClick(MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.play_theme_song:
+                Toast.makeText(MainActivity.this, "Song Playing", Toast.LENGTH_LONG);
+                return true;
+            case R.id.textview_bottom:
+                Toast.makeText(MainActivity.this, "You Clicked Song Name", Toast.LENGTH_LONG);
+                return true;
+            case R.id.display_info:
+                Toast.makeText(MainActivity.this, "Info Displayed Here", Toast.LENGTH_LONG);
+                return true;
 
 
+        }
+        return false;
+    }
+ */
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
